cb7afc9b409719c6704610547108f4e256d9e93e
Yohan Jarosz <yohanjarosz@yahoo.fr>
2013-05-24 08:42:56 +0200

    adding some aliases

---
+++ custom/plugins/alias/alias.plugin.zsh
@@ -0,0 +1,31 @@
+### User specific aliases and functions ###
+alias e='emacs -nw'
+alias se="sudo emacs -nw"
+alias i="ipython"
+
+### useful functions
+function extract () {
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xjf $1     ;;
+        *.tar.gz)    tar xzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xf $1      ;;
+        *.tbz2)      tar xjf $1     ;;
+        *.tgz)       tar xzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
+
+function eclean() {
+    ZSH_ECLEAN_PLACES=${*:-'.'}
+    find ${ZSH_ECLEAN_PLACES} -type f -name "*~" -delete
+}
